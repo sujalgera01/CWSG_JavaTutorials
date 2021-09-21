27bbce58e21241419984cfdd1e77506d7a16ab98
sujalgera01 <sujalgera01@gmail.com>
2021-09-21 23:44:10 +0530

    JAVA Practice Set

---
+++ java practice set1/greet.java
@@ -0,0 +1,10 @@
+public class Three {
+ 
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+        System.out.println("\t\t"+args[0]+" "+args[1]);
+        System.out.println("\t\t\tGood Morning.. Welcome to \"Java Lab\" :)");
+
+
+    }
+}
---
+++ java practice set1/patternq.java
@@ -0,0 +1,20 @@
+import java.lang.*;
+ public class PatternQ
+ {
+  public static void main(String[] args)
+ 	 {
+     	System.out.println("         '__'");
+		System.out.println();
+		System.out.println("         (@@)");
+		System.out.println();
+    	System.out.println("  /========\\/");
+		System.out.println();
+   	    System.out.println("  / || %% ||");
+	    System.out.println();
+   	    System.out.println("*   ||----||");
+	    System.out.println();
+  	    System.out.println("   ##    ##");
+
+  System.out.println("   \"\"    \"\"");
+  }
+ }
