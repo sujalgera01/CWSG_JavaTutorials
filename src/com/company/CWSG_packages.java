8a6ebac30836b5ee5c233975560f703b156cd825
sujalgera01 <sujalgera01@gmail.com>
2021-10-18 17:43:07 +0530

    JAVA

---
+++ src/com/company/CWSG_packages.java
@@ -0,0 +1,13 @@
+package com.company;
+
+//import java.util.Scanner;
+import java.util.*;
+
+public class CWSG_packages {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println("This is a Scanner Class taking input as : " + a);
+//        java.util.Scanner sc = new java.util.Scanner(System.in);
+    }
+}
